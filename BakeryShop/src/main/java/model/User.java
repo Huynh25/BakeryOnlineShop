daca41475391a0a91bece1f8402ebb52ebc7210b
TAn1512 <nguyentruongan15122003@gmail.com>
2024-02-19 16:59:35 +0700

    Create DAO

---
+++ BakeryShop/src/main/java/model/User.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author ACER PC
+ */
+public class User {
+    private String username;
+    private String password;
+    private String role;
+
+    public User() {
+    }
+
+    public User(String username, String pasword, String role) {
+        this.username = username;
+        this.password = pasword;
+        this.role = role;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" + "username=" + username + ", password=" + password + ", role=" + role + '}';
+    }
+    
+    
+}
